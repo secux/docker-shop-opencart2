0f11d079adb71d07408b91a22f264de30e32408b
Mircea Silviu Itu <decemvre@gmail.com>
2016-02-02 13:34:43 +0200

    upd

--- data/install.sh
+++ data/install.sh
@@ -1,1 +1,11 @@
  
+#!/bin/sh
+
+echo "Running install.sh for $1"
+
+echo "Cloning opencart repo"
+# add version
+git clone https://github.com/opencart/opencart.git /www
+
+echo "Installing composer packages"
+cd /www && composer.phar install --ignore-platform-reqs
